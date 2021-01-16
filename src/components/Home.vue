e6bfd443ee1ef151ce11679cf2276279cc2b777c
Princewill Chiaka <9r1nc3w1ll@gmail.com>
2021-01-16 16:32:47 +0100

    chore: Design basic hero layout

---
+++ src/components/Home.vue
@@ -0,0 +1,218 @@
+<template>
+  <div class="homepage">
+    <div class="hero w-full bg-primary">
+      <div class="container mx-auto p-3 md:px-7">
+        <nav class="w-full relative flex flex-wrap text-white items-center py-3">
+          <a class="logo mr-auto lg:mr-4">
+            <span class="hidden text-xl lg:inline-block">TransferWise</span>
+          </a>
+          <a class="link mx-3 hidden lg:block">Personal</a>
+          <div class="divider mx-3 hidden lg:block"></div>
+          <a class="link mx-3 hidden lg:block mr-auto">Business</a>
+          <a class="link mx-3 hidden lg:block">Money transfer</a>
+          <a class="link mx-3 hidden lg:block">Multi-currency account</a>
+          <a class="link mx-3 hidden lg:block">Help</a>
+          <a class="link mx-3 hidden lg:block">EN</a>
+          <a class="link mx-3 hidden lg:block">Login</a>
+          <a class="btn btn-outine-primary mx-3 hidden">Register</a>
+        </nav>
+        <div class="w-full flex flex-wrap mt-32 mb-40">
+          <div class="w-full lg:w-6/12 mr-auto text-white">
+            <h1 class="text-2xl lg:text-5xl font-bold lg:w-10/12">Large transfers, designed to save you money<span class="text-blue-300">.</span></h1>
+            <p class="my-8 lg:w-9/12">TransferWise takes the stress out of sending large amounts of money abroad, helping you save for the important things.</p>
+            <div class="w-full"><a class="btn btn-lg btn-outine-primary">See how we send money</a></div>
+            <div class="w-full flex flex-wrap mt-10">
+              <div class="w-1/2">
+                <div class="mb-5" style="height: 70px; max-width: 80px; object-fit: contain;">
+                  <svg id="el_H8G--UHWO" class="tw-trust-element__image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 84 50"><path d="M14.3 43.5h46.9v-21H0v21h7.1V50l7.2-6.5z" id="el_o50-IfnOhY"></path><g id="el_ZKUtuKmlJ2_an_27_VJB_eK" data-animator-group="true" data-animator-type="2"><path d="M70.1 38.5H7.2v-24h77.2v24h-7.1V45l-7.2-6.5z" id="el_ZKUtuKmlJ2"></path></g><g id="el_cKmWEw9KDj_an_Al496FKhj" data-animator-group="true" data-animator-type="2"><path d="M24 25.5h-3.2l-1-3-1 3h-3.2l2.6 1.9-1 3.1 2.6-1.9 2.6 1.9-1-3.1 2.6-1.9z" id="el_cKmWEw9KDj"></path></g><g id="el_FrN2weN1HV_an_VyY4hnoQje" data-animator-group="true" data-animator-type="2"><path id="el_FrN2weN1HV" d="M37 25.5h-3.2l-1-3-1 3h-3.2l2.6 1.9-1 3.1 2.6-1.9 2.6 1.9-1-3.1z"></path></g><g id="el_QxWDtUDnYN_an_fi3jS4kw1" data-animator-group="true" data-animator-type="2"><path id="el_QxWDtUDnYN" d="M50 25.5h-3.2l-1-3-1 3h-3.2l2.6 1.9-1 3.1 2.6-1.9 2.6 1.9-1-3.1 2.6-1.9z"></path></g><g id="el_0LSJko48lh_an_2TS1w_u6J" data-animator-group="true" data-animator-type="2"><path id="el_0LSJko48lh" d="M63 25.5h-3.2l-1-3-1 3h-3.2l2.6 1.9-1 3.1 2.6-1.9 2.6 1.9-1-3.1 2.6-1.9z"></path></g><g id="el_Yv2r1TIYxk_an_GdS0Y6cG7" data-animator-group="true" data-animator-type="2"><path id="el_Yv2r1TIYxk" d="M76 25.5h-3.2l-1-3-1 3h-3.2l2.6 1.9-1 3.1 2.6-1.9 2.6 1.9-1-3.1 2.6-1.9z"></path></g></svg>
+                </div>
+                <div class="font-bold">Over 9 million customers</div>
+                <div class="font-bold">Read on Trustpilot</div>
+              </div>
+              <div class="w-1/2">
+                <div class="mb-5" style="height: 70px; max-width: 80px; object-fit: contain;">
+                  <svg viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg" class="tw-trust-element__image"><path d="M39.586 46.567H.269v.998h39.317v-.998z" fill="url(#paint0_linear)"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M33.818 12.68H23.36l.582-1.383H34.4l-.582 1.383z" fill="#7CD9F5"></path><path d="M34.456 7.282H5.4a.927.927 0 00-.927.927v38.186c0 .512.415.927.927.927h29.056a.927.927 0 00.927-.927V8.21a.927.927 0 00-.928-.927z" fill="#DBF4FE"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M29.546 37.992H9l.59-1.375h20.546l-.59 1.375zM29.546 30.992H9l.59-1.375h20.546l-.59 1.375zM29.546 23.992H9l.59-1.375h20.546l-.59 1.375zM19.466 16.383H9L9.59 15h10.457l-.581 1.383z" fill="#7CD9F5"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M28.482 17H17l1.539-3H30l-1.518 3z" fill="#7CD9F5"></path><path d="M45.157 39.373l2.3-1.673L36.735 22.96l-2.3 1.673 10.722 14.741z" fill="#253655"></path><path d="M47.836 40.352a2.184 2.184 0 00.48-3.04l-7.426-9.948-3.512 2.562 7.417 9.955a2.184 2.184 0 003.04.471z" fill="#485CC7"></path><path d="M34.884 26.537l3.514-2.556-.939-1.29-3.514 2.556.939 1.29z" fill="#253655"></path><path opacity=".7" d="M28.405 23.947c5.611 0 10.16-4.549 10.16-10.16 0-5.61-4.549-10.16-10.16-10.16-5.61 0-10.159 4.55-10.159 10.16 0 5.611 4.549 10.16 10.16 10.16z" fill="#fff"></path><path d="M18.254 20.514a12.186 12.186 0 103.473-16.886 12.194 12.194 0 00-3.473 16.886zm2.255-1.485a9.492 9.492 0 1113.153 2.71 9.5 9.5 0 01-13.153-2.71z" fill="#fff"></path><path d="M17.547 21.024a13.043 13.043 0 1021.78-14.357 13.043 13.043 0 00-21.78 14.357zm2.404-1.572a10.159 10.159 0 1114.072 2.892 10.167 10.167 0 01-14.072-2.947v.056z" fill="#253655"></path><defs><linearGradient id="paint0_linear" x1=".269" y1="47.07" x2="39.586" y2="47.07" gradientUnits="userSpaceOnUse"><stop stop-opacity="0"></stop><stop offset=".07" stop-opacity=".16"></stop><stop offset=".5" stop-opacity=".3"></stop><stop offset=".93" stop-opacity=".16"></stop><stop offset="1" stop-opacity="0"></stop></linearGradient></defs></svg>
+                </div>
+                <div class="font-bold">FCA regulated</div>
+                <div class="font-bold">Learn more</div>
+              </div>
+            </div>
+          </div>
+          <div class="w-full lg:w-5/12 my-10">
+            <div class="w-full">
+              <div class="funds-input-group">
+                <div class="label">You send</div>
+                <input autocomplete="off" value="1,000">
+                <div class="action-btn">
+                  <div>GBP</div>
+                </div>
+              </div>
+            </div>
+            <ul class="calculator-breakdown lg:w-7/12 pr-5">
+              <li>
+                <span class="sign">-</span>
+                <div class="mr-3 lg:mr-auto">5.53 GBP</div>
+                <div>Low cost transfer</div>
+              </li>
+              <li>
+                <span class="sign">=</span>
+                <div class="mr-3 lg:mr-auto">994.47 GBP</div>
+                <div>Amount we’ll convert</div>
+              </li>
+              <li>
+                <span class="sign">&times;</span>
+                <div class="mr-3 lg:mr-auto">527.079</div>
+                <div>Guaranteed rate (43 hrs)</div>
+              </li>
+            </ul>
+            <div class="w-full mt-2">
+              <div class="funds-input-group">
+                <div class="label">Recipients gets</div>
+                <input autocomplete="off" value="524,164.25">
+                <div class="action-btn">
+                  <div>NGN</div>
+                </div>
+              </div>
+            </div>
+            <div class="w-full mt-10">
+              <a class="btn w-full btn-lg btn-success">Get started</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .lg\:text-5xl {
+    font-size: 2.625rem;
+    line-height: 1.14285714;
+  }
+
+  .hero {
+    background-image: url('../assets/img/hero-bg.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    margin-bottom: 10rem;
+  }
+
+  nav {
+    font-size: 0.9rem;
+  }
+
+  .logo {
+    font-size: 1.1rem;
+  }
+
+  .divider {
+    width: 1px;
+    background-color: white;
+    height: 25px;
+  }
+
+  .link {
+    @apply hover:text-blue-300 font-semibold cursor-pointer;
+  }
+
+  .funds-input-group {
+    width: 100%;
+    display: flex;
+    border: 1px solid transparent;
+    font-size: 20px;
+    font-weight: bold;
+    position: relative;
+    border-radius: 5px;
+    overflow: hidden;
+    z-index: 2;
+  }
+
+  .funds-input-group:hover,
+  .funds-input-group:active {
+    border: 1px solid white;
+  }
+
+  .funds-input-group .label {
+    position: absolute;
+    top: 10px;
+    left: 15px;
+    font-weight: 200;
+    color: #6f8691;
+    font-size: 0.95rem;
+    min-width: 0;
+    pointer-events: none;
+  }
+
+  .funds-input-group input {
+    border: none;
+    outline: none;
+    flex: 1;
+    padding-top: 35px;
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-bottom: 15px;
+    color: #2e4369;
+    min-width: 0;
+  }
+
+  .funds-input-group .action-btn {
+    padding: 10px 30px;
+    background-color: #2e4369;
+    display: flex;
+    align-items: center;
+    color: white;
+    cursor: pointer;
+  }
+
+  .calculator-breakdown {
+    color: white;
+    display: block;
+    position: relative;
+    margin-left: 50px;
+  }
+
+  .calculator-breakdown li {
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-size: 0.9rem;
+  }
+
+  .calculator-breakdown li .sign {
+    width: 1.5rem;
+    height: 1.5rem;
+    background-color: #2e4369;
+    display: grid;
+    place-content: center;
+    border-radius: 50%;
+    font-size: 0.9rem;
+    line-height: 1;
+    position: absolute;
+    left: -35px;
+    z-index: 2;
+    padding: 10px;
+  }
+
+  .calculator-breakdown li::after {
+    content: '';
+    display: block;
+    position: absolute;
+    height: calc(100% + 16px);
+    width: 2px;
+    left: -23px;
+    top: -5px;
+    background-color: #2e4369;
+    z-index: 1;
+  }
+</style>
